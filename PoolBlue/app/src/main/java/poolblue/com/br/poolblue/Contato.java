fc2cec05bba693a34e0e5a097543d2f6efd59485
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2017-12-04 20:41:13 -0200

    Adicionando projeto com SQLite - lista de usuários

--- PoolBlue/app/src/main/java/poolblue/com/br/poolblue/Contato.java
+++ PoolBlue/app/src/main/java/poolblue/com/br/poolblue/Contato.java
@@ -5,4 +5,60 @@ package poolblue.com.br.poolblue;
  */
 
 public class Contato {
+
+    private String nome;
+    private String endereco;
+    private String empresa;
+    private String sexo;
+    private String cargo;
+
+    public Contato(String nome, String endereco, String empresa, String sexo, String cargo) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.empresa = empresa;
+        this.sexo = sexo;
+        this.cargo = cargo;
+    }
+
+    //getter
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+    //setter
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
 }
--- PoolBlue/app/src/main/java/poolblue/com/br/poolblue/DBHelp.java
+++ PoolBlue/app/src/main/java/poolblue/com/br/poolblue/DBHelp.java
@@ -1,8 +1,97 @@
 package poolblue.com.br.poolblue;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by rafaelcarvalho on 04/12/2017.
  */
 
 public class DBHelp {
+
+    private static final String DATABASE = "banco.db";
+    private static final int VERSION = 1;
+    private static final String TABLE_NAME = "contato";
+
+    private Context context;
+
+    private SQLiteDatabase db;
+    private SQLiteStatement ss;
+
+    private static final String INSERT = "INSERT INTO "+ TABLE_NAME +" (nome, endereco, empresa, sexo, cargo) VALUES (?, ?, ?, ?, ?)";
+
+    public DBHelp(Context context) {
+        this.context = context;
+        OpenHelper openHelper = new OpenHelper(this.context);
+        this.db = openHelper.getWritableDatabase();
+        this.ss = this.db.compileStatement(INSERT);
+
+    }
+    public long insert (String nome, String endereco, String empresa, String sexo, String cargo){
+        this.ss.bindString(1, nome);
+        this.ss.bindString(2, endereco);
+        this.ss.bindString(3, empresa);
+        this.ss.bindString(4, sexo);
+        this.ss.bindString(5, cargo);
+        return this.ss.executeInsert();
+    }
+    public void delete(){
+        this.db.delete(TABLE_NAME, null, null);
+    }
+    public List<Contato> getAll(){
+
+        ArrayList<Contato> itens = new ArrayList<>();
+        try {
+            Cursor c = this.db.query(TABLE_NAME, new String[]{"nome", "Endereço", "Empresa", "Sexo", "Cargo"},
+                    null, null,null, null, null, null);
+            int nreg = c.getCount();
+            if(nreg != 0){
+                c.moveToFirst();
+                do {
+                    Contato contato = new Contato(c.getString(0), c.getString(1), c.getString(2), c.getString(3), c.getString(4));
+                    itens.add(contato);
+
+                }while (c.moveToNext());
+                if(c != null && c.isClosed()){
+                    c.close();
+                    return itens;
+                }
+            }else{
+                return null;
+            }
+
+
+        }catch (SQLException e){
+            return null;
+        }
+    }
+    private static class OpenHelper extends SQLiteOpenHelper{
+
+
+        public OpenHelper(Context context) {
+            super(context, DATABASE, null, VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+            String sql = "CREATE TABLE IF NOT EXISTS "+ TABLE_NAME +" (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(50)," +
+                    "endereco VARCHAR(50), empresa VARCHAR(20), sexo VARCHAR(10), cargo VARCHAR(20))";
+            db.execSQL(sql);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int antigaV, int novaV) {
+            db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+            onCreate(db);
+
+        }
+    }
 }
