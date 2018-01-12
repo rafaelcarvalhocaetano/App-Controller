30b935566640359e15893fac5e7cabc6e3f60f57
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2018-01-11 22:55:27 -0200

    Add App CASH

--- Cash/app/src/main/java/io/mda/cash/CadastroActivity.java
+++ Cash/app/src/main/java/io/mda/cash/CadastroActivity.java
@@ -1,8 +1,20 @@
 package io.mda.cash;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 /**
  * Created by rafael on 07/01/18.
@@ -15,5 +27,7 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+
     }
 }
